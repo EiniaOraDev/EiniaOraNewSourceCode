632dd1b0504b055e97257b7459d48dfa3da96947
EiniaOraDev <einiaora@hotmail.com>
2015-07-30 22:32:15 -0400

    changes for wallets
    changes for wallets

--- src/version.h
+++ src/version.h
@@ -30,14 +30,14 @@ static const int DATABASE_VERSION = 70507;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 70019;
+static const int PROTOCOL_VERSION = 70018;
 
 // earlier versions not supported as of Feb 2012, and are disconnected
 static const int MIN_PROTO_VERSION = 209;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
-static const int CADDR_TIME_VERSION = 70019;
+static const int CADDR_TIME_VERSION = 31402;
 
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 60002;
@@ -51,6 +51,6 @@ static const int MEMPOOL_GD_VERSION = 60002;
 
 #define DISPLAY_VERSION_MAJOR       1
 #define DISPLAY_VERSION_MINOR       1
-#define DISPLAY_VERSION_REVISION    2
+#define DISPLAY_VERSION_REVISION    1
 
 #endif
